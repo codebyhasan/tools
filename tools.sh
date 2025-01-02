d73f6c89c2029d94e7ef3bb798119fa025d6dec4
codebyhasan <CodeByHasan@proton.me>
2025-01-03 05:02:47 +0600

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -33,7 +33,7 @@ sudo apt install -y \
 echo "Installing speedtest-cli..."
 sudo apt-get install curl && curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash && sudo apt-get install speedtest
 
-pip install yt-dlp
+sudo pip install yt-dlp
 
 # Clean up
 echo "Cleaning up..."
